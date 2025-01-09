a5f0e4e6b69f8676dad0d2b2e5256afaca37a301
kimmmm126 <rlarkgml88@gmail.com>
2025-01-09 17:29:16 +0900

    modal 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import Link from 'next/link';
 import { useState } from 'react';
 
-interface InfoDataType {
+interface infoDataProps {
   idx: number;
   name: string;
   src: string;
@@ -46,14 +46,18 @@ const Page = () => {
   const [openPopup, setOpenPopup] = useState(false);
 
   const onOpenPopup = () => {
-    setOpenPopup(openPopup);
+    setOpenPopup(true);
+  };
+
+  const closeClick = () => {
+    setOpenPopup(false);
   };
   return (
     <>
       <div className='wrap'>
         <div className='iconWrap'>
           <ul>
-            {iconData.map(({ idx, name, src, className }: InfoDataType) => {
+            {iconData.map(({ idx, name, src, className }: infoDataProps) => {
               return (
                 <li key={idx} className='icon'>
                   <Link
@@ -70,6 +74,30 @@ const Page = () => {
             })}
           </ul>
         </div>
+        {openPopup && (
+          <div className='window modal md'>
+            <div className='title-bar'>
+              <div className='title-bar-text'>
+                <img className='icon' src='http://via.placeholder.com/15x15' alt='' />
+                <h2>Instagram.exe</h2>
+              </div>
+              <div className='title-bar-controls'>
+                <button aria-label='Minimize' />
+                <button aria-label='Maximize' />
+                <button aria-label='Close' onClick={closeClick} />
+              </div>
+            </div>
+            <div className='window-body'>
+              <div className='content'>
+                <h2 className='contentTitle'>@kimmmm126</h2>
+                <div className='divider' />
+                <div className='contentDiv'>
+                  <div className='profile'></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
